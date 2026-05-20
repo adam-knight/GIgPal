8d32d2f3551f20bbf78e36d163afee92430995b0
Adam <adam.elaraby@gmail.com>
2026-05-20 18:55:04 +0100

    Initial Commit

---
+++ GIgPal/GIgPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIgPalApp.swift
+//  GIgPal
+//
+//  Created by Adam on 20/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct GIgPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GIgPalTests/GIgPalTests.swift
@@ -0,0 +1,19 @@
+//
+//  GIgPalTests.swift
+//  GIgPalTests
+//
+//  Created by Adam on 20/05/2026.
+//
+
+import Testing
+@testable import GIgPal
+
+struct GIgPalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
